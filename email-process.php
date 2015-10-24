cbab7ec6880fcdccd1fb59f2d784114bc82c1a3e
Diego Ruiz <dieg.ruiz098@gmail.com>
2015-10-24 02:03:40 -0400

    switching phpmailer for mailgun

--- email-process.php
+++ email-process.php
@@ -1,23 +1,25 @@
 <?php
-require 'includes/PHPMailer-master/class.phpmailer.php';
-$body = $_REQUEST['email_body'];
+# Include the Autoloader (see "Libraries" for install instructions)
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
 
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->Host = "localhost";
-$mail->SMTPAuth = true;     // turn on SMTP authentication
-$mail->Username = "diegruiz";        // Make sure to replace this with your shell enabled user
-$mail->Password = "class08";
-$mail->From = $_REQUEST['email'];
-$mail->FromName = $_REQUEST['name'];
-$mail->addAddress('dieg.ruiz098@gmail.com', 'Diego Ruiz');
-$mail->isHTML(true);
-$mail->Subject= "Diegruiz.com Contact Form Submission - ".$_REQUEST['subject'];
-$mail->Body =$body;
-if(!$mail->send()) {
+# Instantiate the client.
+$mgClient = new Mailgun('key-d244944ffe2edf1a0d95b54756474cd1');
+$domain = "sandbox350b360240074869bee001fc443387d5.mailgun.org";
+
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+                  array('from'    => $_REQUEST['name'].' <'.$_REQUEST['email'].'>',
+                        'to'      => 'Diego Ruiz <dieg.ruiz098@gmail.com>',
+                        'subject' => "Diegruiz.com Contact Form Submission - ".$_REQUEST['subject'],
+                        'text'    => $body));
+
+if(!$result) {
 	echo '<div class="contact_return_msg_error">Message could not be sent. Email at dieg.ruiz098@gmail.com</div>';
 	//echo 'Mailer Error: ' . $mail->ErrorInfo;
 	//exit;
 }
-echo "<div class='contact_return_msg_success'>Message sent! I'll get in contact soon</div>";
+else{
+	echo "<div class='contact_return_msg_success'>Message sent! I'll get in contact soon</div>";
+}
 ?>
